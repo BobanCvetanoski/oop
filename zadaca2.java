8fca9e020a4f9731b29085a1248ecaf5dea51e08
Naum Trpezanovski <trpezanovski.naum@uklo.edu.mk>
2022-11-06 20:18:05 +0100

    Dodavanje na zadaca2

---
+++ zadaca2.java
@@ -0,0 +1,44 @@
+class Vraboten {
+    public String Ime;
+    public String Prezime;
+
+    public String Plata;
+
+
+    public Vraboten(String ime, String prezime, String plata) {
+        Ime = ime;
+        Prezime = prezime;
+        Plata = plata;
+    }
+
+    public String getIme() {
+        return Ime;
+    }
+
+    public void setIme(String ime) {
+        this.Ime = Ime;
+    }
+
+    public String getPrezime() {
+        return Prezime;
+    }
+
+    public void setPrezime(String prezime) {
+        this.Prezime = Prezime;
+    }
+
+    public String getPlata() {
+        return Plata;
+    }
+
+    public void setMatBr(String plata) {
+        Plata = plata;
+    }
+
+}
+public class Main {
+    public static void main(String[] args) {
+        Vraboten nov = new Vraboten("Naum" ,"Trpezanovski" , "45000");
+        System.out.println("Imeto na vraboteniot e: " + nov.Ime + "\n" + "Prezimeto na vraboteniot e: " + nov.Prezime + "\n" + "Platata na vraboteniot iznesuva:  " + nov.Plata );
+    }
+}
